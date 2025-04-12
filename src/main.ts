b616d5d9d6efa8e1bf9ee6f5f3af452322c6c534
Caroline <c.jungefalk@gmail.com>
2025-04-12 22:09:25 +0200

    hämtat in html-element

--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,10 @@ progression: string,
 syllabus: string,
 };
 
+//Hämta element
+let codeInput = document.getElementById("code") as HTMLInputElement;
+let nameInput = document.getElementById("name") as HTMLInputElement;
+let progressionInput = document.getElementById("Progression") as HTMLInputElement;
+let syllabysInput = document.getElementById("syllabus") as HTMLInputElement;
+let buttonEl = document.getElementById("submit") as HTMLButtonElement;
+let courseListEl = document.getElementById("courseList") as HTMLUListElement;
